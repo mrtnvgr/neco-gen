0284331b1a8a4fdd8d8e254b17502ef6455e2b6f
mrtnvgr <martynovegorOF@yandex.ru>
2022-02-07 08:08:07 +0700

    v0.0.2 release code

--- necov2.py
+++ necov2.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
+
 from random import randint
-import cv2
+import cv2, sys
 
 # TODO!
 """
@@ -9,14 +11,52 @@ add custom colors mode
 
 I = 0
 
-print("Necoarc generator v0.0.1")
+print("Necoarc generator v0.0.2")
 print()
-number = int(input("Number: "))
+print(" Mode: ")
+print(" 1) Random")
+print(" 2) Custom")
+modch = input("Mode: ")
+
+if modch=="1":
+    number = int(input("Number: "))
+elif modch=="2":
+    number = 1
+    boots = input("Boots: ")
+    hair = input("Hair: ")
+    hair_sub = input("Hair sub: ")
+    left_eye = input("Left eye: ")
+    right_eye = input("Right eye: ")
+    shirt = input("Shirt: ")
+    skirt = input("Skirt: ")
+else:
+    sys.exit(0)
+
 print("Wait...")
+
 for i in range(number):
     newarc = cv2.imread("input/body.png")
     for part in ["boots", "hair", "hair_sub", "left_eye", "right_eye", "shirt", "skirt"]:
-        cr, cg, cb = (randint(0,255), randint(0,255), randint(0,255))
+        if modch=="1":
+            cr, cg, cb = (randint(0,255), randint(0,255), randint(0,255))
+        elif modch=="2":
+            if part=="boots":
+                curcolors = boots.split(" ")
+            elif part=="hair":
+                curcolors = hair.split(" ")
+            elif part=="hair_sub":
+                curcolors = hair_sub.split(" ")
+            elif part=="left_eye":
+                curcolors = left_eye.split(" ")
+            elif part=="right_eye":
+                curcolors = right_eye.split(" ")
+            elif part=="shirt":
+                curcolors = shirt.split(" ")
+            elif part=="skirt":
+                curcolors = skirt.split(" ")
+            cr = int(curcolors[0])
+            cg = int(curcolors[1])
+            cb = int(curcolors[2])
         picture = cv2.imread("input/" + part + ".png")
         if cr+cg+cb!=0:
             height, width = tuple(picture.shape[1::-1])
@@ -24,7 +64,7 @@ for i in range(number):
                 for y in range(height):
                     r, g, b = picture[x, y]
                     if r!=0 and g!=0 and b!=0:
-                        picture[x, y] = cr-r, cg-g, cb-b
+                        picture[x, y] = r-cr, g-cg, b-cb
         newarc = cv2.add(newarc, picture)
     cv2.imwrite('output/' + str(I+1) + '.png', newarc)
     I += 1
