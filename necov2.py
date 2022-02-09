1a0ee054c64afa84ce52b5d9abc3581595395cbf
mrtnvgr <martynovegorOF@yandex.ru>
2022-02-09 17:42:37 +0700

    v0.0.3 release code (algoritm rewrite ;P)

--- necov2.py
+++ necov2.py
@@ -6,12 +6,9 @@ import cv2, sys
 # TODO!
 """
 add all colors mode
-add custom colors mode
 """
 
-I = 0
-
-print("Necoarc generator v0.0.2")
+print("Necoarc generator v0.0.3")
 print()
 print(" Mode: ")
 print(" 1) Random")
@@ -22,13 +19,13 @@ if modch=="1":
     number = int(input("Number: "))
 elif modch=="2":
     number = 1
-    boots = input("Boots: ")
-    hair = input("Hair: ")
-    hair_sub = input("Hair sub: ")
-    left_eye = input("Left eye: ")
-    right_eye = input("Right eye: ")
-    shirt = input("Shirt: ")
-    skirt = input("Skirt: ")
+    boots = int(input("Boots(0-180): "))
+    hair = int(input("Hair(0-180): "))
+    hair_sub = int(input("Hair sub(0-180): "))
+    left_eye = int(input("Left eye(0-180): "))
+    right_eye = int(input("Right eye(0-180): "))
+    shirt = int(input("Shirt(0-180): "))
+    skirt = int(input("Skirt(0-180): "))
 else:
     sys.exit(0)
 
@@ -37,35 +34,31 @@ print("Wait...")
 for i in range(number):
     newarc = cv2.imread("input/body.png")
     for part in ["boots", "hair", "hair_sub", "left_eye", "right_eye", "shirt", "skirt"]:
-        if modch=="1":
-            cr, cg, cb = (randint(0,255), randint(0,255), randint(0,255))
-        elif modch=="2":
-            if part=="boots":
-                curcolors = boots.split(" ")
-            elif part=="hair":
-                curcolors = hair.split(" ")
-            elif part=="hair_sub":
-                curcolors = hair_sub.split(" ")
-            elif part=="left_eye":
-                curcolors = left_eye.split(" ")
-            elif part=="right_eye":
-                curcolors = right_eye.split(" ")
-            elif part=="shirt":
-                curcolors = shirt.split(" ")
-            elif part=="skirt":
-                curcolors = skirt.split(" ")
-            cr = int(curcolors[0])
-            cg = int(curcolors[1])
-            cb = int(curcolors[2])
         picture = cv2.imread("input/" + part + ".png")
-        if cr+cg+cb!=0:
-            height, width = tuple(picture.shape[1::-1])
-            for x in range(width):
-                for y in range(height):
-                    r, g, b = picture[x, y]
-                    if r!=0 and g!=0 and b!=0:
-                        picture[x, y] = r-cr, g-cg, b-cb
-        newarc = cv2.add(newarc, picture)
-    cv2.imwrite('output/' + str(I+1) + '.png', newarc)
-    I += 1
-    print("Neco arc: " + str(I) + "/" + str(number))
+        hsv = cv2.cvtColor(picture, cv2.COLOR_RGB2HSV)
+        h = hsv[:,:,0]
+        s = hsv[:,:,1]
+        v = hsv[:,:,2]
+        if modch=="2":
+            if part=="boots":
+                huechange = boots
+            elif part=="hair":
+                huechange = hair
+            elif part=="hair_sub":
+                huechange = hair_sub
+            elif part=="left_eye":
+                huechange = left_eye
+            elif part=="right_eye":
+                huechange = right_eye
+            elif part=="shirt":
+                huechange = shirt
+            elif part=="skirt":
+                huechange = skirt
+        elif modch=="1":
+            huechange = randint(0,180)
+        hnew = cv2.add(h, huechange)
+        hsvnew = cv2.merge([hnew,s,v])
+        result = cv2.cvtColor(hsvnew, cv2.COLOR_HSV2RGB)
+        newarc = cv2.add(newarc, result)
+    cv2.imwrite('output/' + str(i+1) + '.png', newarc)
+    print("Neco arc: " + str(i+1) + "/" + str(number))
